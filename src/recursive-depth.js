68e12343313d40bbe3c280a5295176e378eccb22
Mantuok <etigiloff@gmail.com>
2021-03-30 00:02:48 +0300

    feat: done vigenere-cipher

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,15 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(arr){
+  calculateDepth(arr) {
     let count = 1;
-    if(!Array.isArray(arr)) throw new Error();
-    for(let elem of arr){
-      if(Array.isArray(elem)){
+    if (!Array.isArray(arr)) throw new Error();
+    for (let elem of arr) {
+      if (Array.isArray(elem)) {
         let elemCount = this.calculateDepth(elem) + 1;
-        if(count < elemCount) count = elemCount;
+        if (count < elemCount) count = elemCount;
       }
-      
     }
     return count;
   }
